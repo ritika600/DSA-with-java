ac77c116f1e2b24d860799b15dbf4ea818c1377e
Ritika <64130468+ritika600@users.noreply.github.com>
2021-11-11 22:42:47 +0530

    Create checkIfContainsX.java

---
+++ Trees/Assignments/checkIfContainsX.java
@@ -0,0 +1,34 @@
+public class Solution {
+
+/*	TreeNode class 
+ * 
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+	
+	public static boolean checkIfContainsX(TreeNode<Integer> root, int x){
+
+		// Write your code here
+        if(root==null){
+            return false;
+        }
+        if(root.data==x){
+            return true;
+        }
+        
+        for(int i=0;i<root.children.size();i++){
+           boolean ans= checkIfContainsX(root.children.get(i),x);
+            if(ans){
+                return true;
+            }
+        }
+        return false;
+	}
+	
+}
