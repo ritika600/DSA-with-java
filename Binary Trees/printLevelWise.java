6b7305d1c0431d2d77b40df090b88da585c84a56
Ritika <64130468+ritika600@users.noreply.github.com>
2021-11-15 22:06:51 +0530

    Create printLevelWise.java

---
+++ Binary Trees/printLevelWise.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Solution {
+	
+	public static void printLevelWise(BinaryTreeNode<Integer> root) {
+		//Your code goes here
+        if (root==null)
+        {
+            return;
+        }
+        Queue<BinaryTreeNode<Integer>> nodesToPrint=new LinkedList<BinaryTreeNode<Integer>>();
+        nodesToPrint.add(root);
+        while(!nodesToPrint.isEmpty())
+        {
+            BinaryTreeNode<Integer> front = nodesToPrint.poll();
+            System.out.print(front.data+":");
+            if (front.left!=null)
+            {
+                nodesToPrint.add(front.left);
+                System.out.print("L:"+front.left.data);
+            }
+            else
+            {
+                System.out.print("L:-1");
+            }
+            
+            if (front.right!=null)
+            {
+                nodesToPrint.add(front.right);
+                System.out.print(",R:"+front.right.data);
+            }
+            else
+            {
+                System.out.print(",R:-1");
+            }
+            System.out.println();
+        }
+	}
+	
+}
